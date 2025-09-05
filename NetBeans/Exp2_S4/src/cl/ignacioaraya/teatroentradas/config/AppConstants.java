4496e266918c7abca338123ec12c6d361e4a2c95
ignacio <iaraya19@hotmail.com>
2025-09-05 11:36:19 -0400

    Create AppConstants.java

---
+++ NetBeans/Exp2_S4/src/cl/ignacioaraya/teatroentradas/config/AppConstants.java
@@ -0,0 +1,11 @@
+package cl.ignacioaraya.teatroentradas.config;
+
+public final class AppConstants {
+    private AppConstants() {}
+
+    public static final int PRECIO_BASE_ASIENTO = 20000;
+    public static final int DESCUENTO_ESTUDIANTE = 10;
+    public static final int DESCUENTO_ADULTO_MAYOR = 15;
+    public static final int EDAD_MAX_ESTUDIANTE = 25;
+    public static final int EDAD_MIN_ADULTO_MAYOR = 65;
+}
