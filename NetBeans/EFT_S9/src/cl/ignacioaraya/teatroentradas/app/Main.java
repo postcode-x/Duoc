6be1e07139478aa896036cdfb73c38d8ca9f0999
ignacio <iaraya19@hotmail.com>
2025-10-08 02:08:42 -0300

    Initial commit

---
+++ NetBeans/EFT_S9/src/cl/ignacioaraya/teatroentradas/app/Main.java
@@ -0,0 +1,11 @@
+package cl.ignacioaraya.teatroentradas.app;
+
+public class Main {
+    
+    public static void main(String[] args){
+    
+        System.out.println("OK");
+        
+    }
+    
+}
